4814ad2102ebd57db86fbcbc3a5312dfe82176c8
Joaquincabreramartinez <118214629+Joaquincabreramartinez@users.noreply.github.com>
2022-11-29 15:54:32 +0100

    hello por wola
    hello por wola

--- NuevoProyectoRama/src/nuevo/HolaMundo.java
+++ NuevoProyectoRama/src/nuevo/HolaMundo.java
@@ -3,10 +3,10 @@ package nuevo;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("Helloo Mundo");
+		System.out.println("wola Mundo");
 	}
 	public static void imprimeSaludo() { 
-		System.out.println(“Hei”);    
+		System.out.println("Hei");    
 		}
 
 
