f2c2ba27f4f4d8871e08f9cffc22245a5c4aa3bc
Usuario <Usuario@DESKTOP-A79ILPI>
2022-11-29 15:48:09 +0100

    push
    push

--- NuevoProyectoRama/src/nuevo/HolaMundo.java
+++ NuevoProyectoRama/src/nuevo/HolaMundo.java
@@ -5,5 +5,9 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		System.out.println("Helloo Mundo");
 	}
+	public static void imprimeSaludo() { 
+		System.out.println(“Hei”);    
+		}
+
 
 }
